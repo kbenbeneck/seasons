bfffdc5dd4cfbb6dc26645898abbc37c4b8b626e
KBENBENECK <kbenbeneck@gmail.com>
2021-05-18 12:40:39 -0400

    Refactor getCurrentPosition and move it from the constructor to componentDidMount lifecycle method.

--- src/index.js
+++ src/index.js
@@ -6,18 +6,16 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { lat: null, errorMessage: '' };        
+    }
 
-        this.state = { lat: null, errorMessage: '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
+
     render() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
