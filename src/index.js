8f5585294832b89311952757a1e047d331893604
KBENBENECK <kbenbeneck@gmail.com>
2021-05-18 11:31:58 -0400

    Implement constructor, reference parant constructor with super() and Initialize state.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+    }
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (err) => console.log(err)
         );
-        return <div>Latitude:</div>
-    }
+        return <div>Latitude:</div>;
+    };
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
